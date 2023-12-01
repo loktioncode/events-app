150a081ecbde816aadc855d9c80acaf97fd35066
Elisha <elishabere4@gmail.com>
2023-12-01 12:25:04 +0200

    add loader

--- components/allEvents.tsx
+++ components/allEvents.tsx
@@ -11,13 +11,36 @@ import Image from 'next/image'
 import { Button } from "./ui/button";
 import Link from "next/link";
 import Loader from "./ui/loader";
+import { ArrowLeftIcon, HeartIcon } from "lucide-react";
 
 
 export default function AllEvents(props: any) {
 
     if (!props.all_events.length) {
-        return <div className="flex justify-center items-center p-8" ><Loader />
-        </div>
+        return (
+            <div className="grid grid-cols-3 gap-4 w-full pt-8 ">
+
+                {[...Array(9)].map((_, i) => 4 + i * 2).map((item: any) => (
+
+                    <div key={item} className="flex justify-center items-center pt-8" >
+                        <div className="flex flex-col rounded shadow-md w-60 sm:w-80 animate-pulse h-96">
+                            <div className="h-48 rounded-t bg-gray-400"></div>
+                            <div className="flex-1 px-4 py-8 space-y-4 sm:p-8 bg-gray-300">
+                                <div className="w-full h-6 rounded bg-gray-300"></div>
+                                <div className="w-full h-6 rounded bg-gray-200"></div>
+                                <div className="w-3/4 h-6 rounded bg-gray-200"></div>
+                            </div>
+                        </div>
+
+                    </div>
+                ))}
+            </div>
+        )
+    }
+
+
+    const bookmarkEvent = (event: any) => {
+
     }
 
     return (
@@ -26,8 +49,14 @@ export default function AllEvents(props: any) {
                 props?.all_events?.map((event: any) => (
 
                     <Card key={event?.id} className="hover:shadow-2xl" >
-                        <CardHeader className="bg-blue-600 text-white mb-4 " style={{ backgroundImage: `url(${event?.artist?.image_url})` }}>
-                            <CardTitle className="text-2xl">{event?.title || 'Event Title'}</CardTitle>
+                        <CardHeader className="bg-blue-600 text-white mb-4 filter-grayscale" style={{ backgroundImage: `url(${event?.artist?.image_url})` }}>
+                            <div className="flex flex-row justify-between">
+                                <CardTitle className="text-2xl">{event?.title || 'Event Title'}</CardTitle>
+                                <Button onClick={() => bookmarkEvent(event)} size={"icon"} variant={"ghost"} >
+                                    <HeartIcon fill="red"></HeartIcon>
+                                </Button>
+
+                            </div>
                             <CardDescription className="text-lg text-white">{new Date(event?.datetime).toLocaleString() || 'Event Date'}</CardDescription>
                         </CardHeader>
                         <CardContent >
--- components/bandInfo.tsx
+++ components/bandInfo.tsx
@@ -3,13 +3,17 @@
 import { useEffect } from "react";
 import Image from 'next/image'
 import { Badge } from "@/components/ui/badge"
+import Loader from "./ui/loader";
 
 export default function BandInfo(props: any) {
     const { all_events } = props;
 
 
     if (all_events.length === 0) {
-        return <div className="flex justify-center items-center p-8" >no artist selected..</div>
+        return <div className="flex justify-center items-center p-8 flex-col" >
+            <Loader />
+            <p className="pt-4 text-lg">Please select an artist</p>
+        </div>
     }
 
     return (
@@ -25,14 +29,14 @@ export default function BandInfo(props: any) {
                 className="flex-shrink-0 object-cover h-64 rounded-sm sm:h-96 dark:bg-gray-500 aspect-square"
             />
             <div>
-                <h2 className="text-2xl font-semibold py-2 text-blue-700">{all_events[0]?.artist?.name}</h2>
-                
+                <h2 className="text-2xl font-semibold py-2 text-b">{all_events[0]?.artist?.name}</h2>
+
                 <div className="flex flex-wrap">
-                   {
-                    all_events[0]?.artist?.links.map((link: any) => (
-                        <a key={link.url} className=" py-2 " href={link.url} target="_blank"><Badge variant="outline" className="text-lg font-normal mx-2 text-blue-700 border-2 border-blue-700" >{link.type}</Badge></a>
-                    ))
-                   }
+                    {
+                        all_events[0]?.artist?.links.map((link: any) => (
+                            <a key={link.url} className=" py-2 " href={link.url} target="_blank"><Badge variant="outline" className="text-lg font-normal mx-2 text-blue-700 border-2 border-blue-700" >{link.type}</Badge></a>
+                        ))
+                    }
                 </div>
             </div>
         </div>
